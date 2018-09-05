b80091746e8d7188f97c802e88526887976bde10
Ferry Temaja Putra <ferry.temaja.putra@gmail.com>
2018-09-05 09:22:08 +0800

    Initial commit

---
+++ InstallerApp.Helper/Greeter.vb
@@ -0,0 +1,5 @@
+﻿Public Class Greeter
+    Public Shared Function Greet() As String
+        Return "Hi"
+    End Function
+End Class
---
+++ InstallerApp/Module1.vb
@@ -0,0 +1,10 @@
+﻿Imports InstallerApp.Helper
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("InstallerApp says '" & Greeter.Greet & "!'")
+        Console.ReadLine()
+    End Sub
+
+End Module
